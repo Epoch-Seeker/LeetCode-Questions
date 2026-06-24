fa83825ee430895e682a757d47008e61020b39f5
Sunny Kumar <sunnyiitian1432@gmail.com>
2026-06-24 14:21:00 +0530

    Time: 20 ms (4.55%), Space: 24.3 MB (5.13%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,28 +1,90 @@
+class Trienode{
+    public:
+        char data;
+        Trienode* children[26];
+        bool isterminal;
+
+        Trienode(char ch){
+            data = ch;
+            for(int i=0;i<26;i++){
+                children[i] = NULL;
+            }
+            isterminal = false;
+        }
+};
+
+class Trie{
+    public:
+        Trienode* root;
+        Trie(){
+            root = new Trienode('\0');
+        }
+
+        void insertutil(Trienode* root , string word , int idx){
+            if(idx == word.size()){
+                root -> isterminal = true;
+                return;
+            }
+
+            char ch = word[idx];
+
+            int index = ch - 'a';
+
+            // char not present
+            if(root -> children[index] == NULL){
+                root -> children[index] = new Trienode(ch);
+            }
+
+            insertutil(root -> children[index] , word , idx + 1);
+        }
+
+        void insert(string word){
+            insertutil(root , word , 0);
+        }
+
+        void get_ansutil(Trienode* root , string& s){
+            int child_count = 0;
+
+            Trienode* child;
+
+            for(int i = 0 ;i< 26 ; i++){
+                if(root -> children[i]){
+                    child = root -> children[i];
+                    child_count++;
+                }
+            }
+
+            if(child_count != 1)return;
+
+            s += child -> data;
+
+            if(child -> isterminal == true)return;
+
+            get_ansutil(child , s);
+        }
+
+
+        void get_ans(string& s){
+             get_ansutil(root , s);
+        }
+
+               
+};
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string res = "";
 
-        if(strs.size() == 1)return strs[0];
-        bool all_zero = true;
         for(auto a : strs){
-            if(a.size() > 0)all_zero = false;
+            if(a == "")return "";
         }
-
-        if(all_zero)return "";
-
-        for(int i = 0 ; i< strs[0].size() ; i++){
-            char ch = strs[0][i];
-            bool he = true;
-            for(int j = 1 ; j< strs.size() ; j++){
-                if(strs[j][i] != ch){
-                    he = false;break;
-                }
-            }
-            if(he) res += ch;
-            else break;
+        Trie t ;
+        for(int i =0 ;i<strs.size() ; i++){
+            t.insert(strs[i]);
         }
+        string ans = "";
+        t.get_ans(ans);
 
-        return res;
+        return ans;
     }
 };
