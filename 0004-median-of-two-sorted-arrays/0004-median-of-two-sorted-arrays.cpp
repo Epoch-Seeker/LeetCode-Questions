cab5305fcd37ab97a21179e927cb54e579eb1a73
Sunny Kumar <sunnyiitian1432@gmail.com>
2026-07-04 21:58:53 +0530

    Time: 3 ms (28.28%), Space: 95.2 MB (61.73%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size(), n = nums2.size();
+        int n = nums1.size();
+        int m = nums2.size();
 
-        // Always binary search on the smaller array
-        if (m > n)
-            return findMedianSortedArrays(nums2, nums1);
+        int i=0,j=0;
 
-        int low = 0, high = m;
+        double m1 = -1, m2 = -1;
 
-        while (low <= high) {
-            int cut1 = (low + high) / 2;
-            int cut2 = (m + n + 1) / 2 - cut1;
-
-            int l1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
-            int l2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
-
-            int r1 = (cut1 == m) ? INT_MAX : nums1[cut1];
-            int r2 = (cut2 == n) ? INT_MAX : nums2[cut2];
-
-            if (l1 <= r2 && l2 <= r1) {
-                if ((m + n) % 2 == 0) {
-                    return (max(l1, l2) + min(r1, r2)) / 2.0;
-                } else {
-                    return max(l1, l2);
-                }
+        for(int t=0;t <= (n+m)/2 ; t++){
+            m2 = m1;
+            if(i < n && j < m){
+                if(nums1[i] < nums2[j]){
+                    m1 = nums1[i++];
+                }else m1 = nums2[j++];
             }
-            else if (l1 > r2) {
-                high = cut1 - 1;
+
+            else if(i < n){
+                m1 = nums1[i++];
             }
-            else {
-                low = cut1 + 1;
+
+            else if(j < m){
+                m1 = nums2[j++];
             }
         }
 
-        return 0.0;
+        if((n+m)%2 ==0){
+            return (m1 + m2)/2.0;
+        }else return m1;
     }
 };
