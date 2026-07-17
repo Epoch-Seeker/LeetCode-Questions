ca03cf2503e291e64eae109add55d9f9fbe237f3
Sunny Kumar <sunnyiitian1432@gmail.com>
2026-07-17 19:01:31 +0530

    Time: 71 ms (74.63%), Space: 93.4 MB (65.52%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
@@ -1,47 +1,107 @@
+class Node{
+    public:
+        int freq;
+        unordered_set<string> keys;
+        Node* next;
+        Node* prev;
+
+        Node(int d){
+            this-> freq = d;
+            this-> prev = NULL;
+            this-> next = NULL;
+        } 
+};
 class AllOne {
 public:
-    unordered_map<string , int> mp;
-    priority_queue<pair<int , string>> maxHeap;
-    priority_queue<pair<int  , string> , vector<pair<int , string>> , greater<pair<int , string>>> minHeap;
-
+    unordered_map<string , Node*> mp;
+    Node* head;
+    Node* tail;
+    void insertAfter(Node* node , Node* newnode){
+        newnode -> next = node -> next;
+        node -> next = newnode;
+        newnode -> prev = node;
+        newnode -> next -> prev = newnode;
+    }
+    void removeNode(Node* node){
+        node -> prev -> next = node -> next;
+        node -> next -> prev = node -> prev;
+        delete node;
+    }
     AllOne() {
-        
+        head = new Node(0);
+        tail = new Node(0);
+        head -> next = tail;
+        tail -> prev = head;
     }
     
     void inc(string key) {
-        mp[key]++;
-        maxHeap.push({mp[key] , key});
-        minHeap.push({mp[key] , key});
+        // new key
+        if(!mp.count(key)){
+            // first key
+            if(head -> next != tail && head -> next -> freq == 1){
+                head -> next -> keys.insert(key);
+            }else{
+            
+                Node* temp = new Node(1);
+                temp -> keys.insert(key);
+                insertAfter(head , temp);
+            }
+            mp[key] = head->next;
+            return;
+        }
+
+        Node* node = mp[key];
+        
+        // next freq already exit
+        if(node -> freq + 1 == node -> next -> freq){
+            node -> next -> keys.insert(key);
+        }else{
+            Node* temp = new Node(node -> freq + 1);
+            temp -> keys.insert(key);
+            insertAfter(node , temp);
+        }
+        mp[key] = node -> next;
+
+        node -> keys.erase(key);
+        if(node -> keys.empty())removeNode(node);
     }
     
     void dec(string key) {
-        mp[key]--;
-        if(mp[key] == 0){
+        Node* node = mp[key];
+
+        if(node -> freq == 1){
+            node -> keys.erase(key);
+            if(node -> keys.empty()){
+                node -> prev -> next = node -> next;
+                node -> next -> prev = node -> prev;
+                delete node;
+            }
             mp.erase(key);
             return;
         }
-        maxHeap.push({mp[key] , key});
-        minHeap.push({mp[key] , key});
+
+        // the dec freq node exit
+        if(node -> freq -1 == node -> prev -> freq){
+            node -> prev -> keys.insert(key);
+        }else{
+            Node* temp = new Node(node -> freq - 1);
+            temp -> keys.insert(key);
+            insertAfter(node -> prev , temp);
+        }
+        mp[key] = node -> prev;
+
+        node -> keys.erase(key);
+        if(node -> keys.empty())removeNode(node);
     }
     
     string getMaxKey() {
-        while(!maxHeap.empty()){
-            auto tp = maxHeap.top();
-            if(mp.count(tp.second) && mp[tp.second] == tp.first){
-                return tp.second;
-            }else maxHeap.pop();
-        }
-        return "";
+        if(tail -> prev == head)return "";
+        return *(tail -> prev -> keys.begin());
     }
     
     string getMinKey() {
-        while(!minHeap.empty()){
-            auto tp = minHeap.top();
-            if(mp.count(tp.second) && mp[tp.second] == tp.first){
-                return tp.second;
-            }else minHeap.pop();
-        }
-        return "";
+        if(head -> next == tail)return "";
+        return *(head -> next -> keys.begin());
     }
 };
 
