a13a86c4f5e27ff0d1ebbaab94031c8cdc16c735
Sunny Kumar <sunnyiitian1432@gmail.com>
2026-07-29 01:01:35 +0530

    Time: 4 ms (51.52%), Space: 15.5 MB (8%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int , int> mp;
+        unordered_map<int , int> mp;
         int a , b;
         for(int i=0;i<nums.size();i++){
             int comp = target - nums[i];
