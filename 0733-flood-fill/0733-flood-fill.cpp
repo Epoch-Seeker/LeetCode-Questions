882cb47937fd1e9489bd4853c19a8aad5a0bdef9
Sunny Kumar <sunnyiitian1432@gmail.com>
2026-08-10 16:33:58 +0530

    Time: 0 ms (100%), Space: 18.7 MB (8.17%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+
+        int tar = image[sr][sc];
+        if(tar == color) return image;
+
         queue<vector<int>> q;
 
         int m = image.size();
@@ -8,13 +12,7 @@ public:
 
         q.push({sr , sc});
 
-        int tar = image[sr][sc];
-
         vector<pair<int , int>> dr = {{0, -1} , {0, 1} , {1 , 0} , {-1 , 0}};
-
-        vector<vector<bool>> vis(m , vector<bool> (n , false));
-
-        vis[sr][sc] = true;
 
         while(!q.empty()){
              
@@ -30,8 +28,7 @@ public:
                 int x = i + d.first;
                 int y = j + d.second;
 
-                if(x >= 0 && x < m && y >= 0 && y < n && image[x][y] == tar && !vis[x][y]){
-                    vis[x][y] = true;
+                if(x >= 0 && x < m && y >= 0 && y < n && image[x][y] == tar){
                     q.push({x , y});
                 }
             }  
