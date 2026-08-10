8ea8f1f2a89eba4571dac866b58ec17580ffe05f
Sunny Kumar <sunnyiitian1432@gmail.com>
2026-08-10 15:20:10 +0530

    Time: 4 ms (24.23%), Space: 21 MB (12.11%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -16,7 +16,7 @@ public:
 
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(isConnected[i][j]){
+                if(isConnected[i][j] && i != j){
                     adj[i+1].push_back(j+1);
                     adj[j+1].push_back(i+1);
                 }
