02e7a03a88513ceb1131ff238d3a83c3fbf62994
Sunny Kumar <sunnyiitian1432@gmail.com>
2026-08-11 13:38:15 +0530

    Time: 11 ms (97.17%), Space: 139.3 MB (25.91%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        multiset<int> st;
-
-        for(int i=0;i<k;i++)st.insert(nums[i]);
-
+        deque<int> q;
         vector<int> ans;
-        ans.push_back(*st.rbegin());
-        for(int i = k ; i<nums.size() ; i++){
-            st.erase(st.find(nums[i-k]));
-            st.insert(nums[i]);
-            ans.push_back(*st.rbegin());
+
+        for(int i=0;i<k;i++){
+            while(!q.empty() && nums[q.back()] <= nums[i])q.pop_back();
+            q.push_back(i);
+        }
+
+        ans.push_back(nums[q.front()]);
+
+        for(int i = k ; i< nums.size() ; i++){
+
+            // remove idx which are no longer needed
+            while(!q.empty() && q.front() <= i - k)q.pop_front();
+
+            while(!q.empty() && nums[q.back()] <= nums[i])q.pop_back();
+            q.push_back(i);
+
+            ans.push_back(nums[q.front()]);
         }
 
         return ans;
