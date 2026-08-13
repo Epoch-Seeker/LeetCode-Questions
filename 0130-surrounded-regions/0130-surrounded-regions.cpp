757f2c7eaf051a926f2ccd68cc69b9e558782a84
Sunny Kumar <sunnyiitian1432@gmail.com>
2026-08-13 17:11:05 +0530

    Time: 0 ms (100%), Space: 13.9 MB (80.87%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& board , int i , int j , vector<pair<int , int>>& temp, bool& flag, vector<vector<bool>>& vis){
-        vis[i][j] = true;
-        temp.push_back({i,j});
+    void dfs(vector<vector<char>>& board , int i , int j ){
+        board[i][j] = 's';
 
         int dx[] = {-1 , 1, 0, 0};
         int dy[] = {0 , 0, -1, 1};
@@ -11,14 +10,9 @@ public:
             int x = i + dx[k];
             int y = j + dy[k];
 
-            if(x < 0 || x >= board.size() || y < 0 || y >= board[0].size()){
-                flag = true;
-                continue;
-            }
+            if(x >=0 && x < board.size() && y>=0 && y < board[0].size() && board[x][y] == 'O')dfs(board , x , y);
 
-            if(vis[x][y] || board[x][y] == 'X')continue;
-
-            dfs(board , x , y , temp , flag , vis);
+            
         }
     }
     void solve(vector<vector<char>>& board) {
@@ -28,25 +22,28 @@ public:
         int m = board.size();
         int n = board[0].size();
 
-        vector<vector<bool>> vis(m , vector<bool>(n , false));
+        // top
+        for(int i=0;i< n ; i++){
+            if(board[0][i] == 'O')dfs(board , 0 , i);
+            if(board[m-1][i] == 'O')dfs(board , m-1 , i);
+        }
+
+        for(int i=0;i< m; i++){
+            if(board[i][0] == 'O')dfs(board , i, 0);
+            if(board[i][n-1] == 'O')dfs(board , i, n-1);
+        }
 
         for(int i=0;i< m ; i++){
             for(int j = 0 ;j < n ; j++){
-                if(board[i][j] == 'O' && !vis[i][j]){
-                    vector<pair<int , int>> temp;
-                    bool flag = false;
-                    dfs(board , i , j , temp , flag , vis);
-                    if(!flag){
-                        v.push_back(temp);
-                    }
+                if(board[i][j] == 'O'){
+                    board[i][j] = 'X';
                 }
+                if(board[i][j] == 's'){
+                    board[i][j] = 'O';
+                }
+                
             }
         }
-
-        for(auto t : v){
-            for(auto s : t){
-                board[s.first][s.second] = 'X';
-            }
-        }
+ 
     }
 };
