0486ad0a045d78395871cf411ae2cd71c0b92722
Sunny Kumar <sunnyiitian1432@gmail.com>
2026-08-22 02:43:59 +0530

    Time: 4 ms (59.93%), Space: 15.8 MB (55.22%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,21 +1,51 @@
 class Solution {
 public:
-    void solve(vector<int>& nums , long long& ans , int i , int& mn , int& mx){
-        if(i>=nums.size())return;
-
-        mn = min(mn , nums[i]);
-        mx = max(mx , nums[i]);
-
-        ans += (long long)(mx - mn);
-
-        solve(nums ,ans , i+1 , mn , mx);
-    }
     long long subArrayRanges(vector<int>& nums) {
         long long ans = 0;
-        for(int i=0;i<nums.size();i++){
-            int mn = INT_MAX;
-            int mx = INT_MIN;
-            solve(nums , ans , i , mn , mx);
+        int n =nums.size();
+        vector<int> leftmin(n , 0) , rightmin(n , 0);
+        vector<int> leftmax(n , 0) , rightmax(n , 0);
+        stack<int> st;
+
+        // previous minimum
+        for(int i=0;i< n ;i++){
+            while(!st.empty() && nums[st.top()] > nums[i])st.pop();
+            leftmin[i] = st.empty() ? i+1 : i - st.top();
+            st.push(i);
+        }
+
+        while(!st.empty())st.pop();
+
+        // next minimum 
+        for(int i= n-1 ; i>=0; i--){
+            while(!st.empty() && nums[st.top()] >= nums[i])st.pop();
+            rightmin[i] = st.empty() ? n-i : st.top()-i;
+            st.push(i);
+        }
+
+        while(!st.empty())st.pop();
+
+        // previous maximum
+        for(int i=0;i< n ;i++){
+            while(!st.empty() && nums[st.top()] < nums[i])st.pop();
+            leftmax[i] = st.empty() ? i+1 : i - st.top();
+            st.push(i);
+        }
+
+        while(!st.empty())st.pop();
+
+        // next minimum 
+        for(int i= n-1 ; i>=0; i--){
+            while(!st.empty() && nums[st.top()] <= nums[i])st.pop();
+            rightmax[i] = st.empty() ? n-i : st.top()-i;
+            st.push(i);
+        }
+
+        for(int i=0;i< n;i++){
+            long long cont = (long long)(leftmax[i]*rightmax[i]);
+            ans += ((long long)nums[i]*cont);
+            cont = (long long)(leftmin[i]*rightmin[i]);
+            ans -= ((long long)nums[i]*cont);
         }
         return ans;
     }
