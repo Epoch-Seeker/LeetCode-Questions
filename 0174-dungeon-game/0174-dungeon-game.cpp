d762dc51076251f209ebda83006d3ccc889b106b
Sunny Kumar <sunnyiitian1432@gmail.com>
2026-08-26 14:53:13 +0530

    Time: 3 ms (19.47%), Space: 13.2 MB (23.86%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.cpp
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& dungeon , int i , int j , vector<vector<int>>& dp){
+    int solve(vector<vector<int>>& dungeon){
         int m = dungeon.size();
         int n = dungeon[0].size();
+        vector<vector<int>> dp(m+1 , vector<int>(n+1 , INT_MAX));
 
-        if(i >= m|| j >= n)return INT_MAX;
+        // if(i >= m|| j >= n)return INT_MAX;
+        dp[m-1][n-1] = max(1 , 1 - dungeon[m-1][n-1]);
 
-        if(i == m-1 && j == n-1)return max(1 , 1 - dungeon[i][j]);
+        // if(i == m-1 && j == n-1)return max(1 , 1 - dungeon[i][j]);
 
-        if(dp[i][j] != -1)return dp[i][j];
+        // if(dp[i][j] != -1)return dp[i][j];
 
-        int down = solve(dungeon , i+1 , j , dp);
-        int right = solve(dungeon , i , j+1 , dp);
+        for(int i=m-1 ; i>=0 ; i--){
+            for(int j=n-1;j>=0;j--){
+                if (i == m-1 && j == n-1)
+                    continue;
 
-        int need = min(down , right);
+                int down = dp[i+1][j];
+                int right = dp[i][j+1];
 
-        return dp[i][j] = max(1 , need - dungeon[i][j]);
+                int need = min(down , right);
+
+                dp[i][j] = max(1 , need - dungeon[i][j]);
+            }
+        }
+
+        return dp[0][0];
     }
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
         
-        int m = dungeon.size();
-        int n = dungeon[0].size();
-        vector<vector<int>> dp(m , vector<int>(n , -1));
-        return solve(dungeon ,0, 0 , dp);
+        
+        return solve(dungeon);
     }
 };
